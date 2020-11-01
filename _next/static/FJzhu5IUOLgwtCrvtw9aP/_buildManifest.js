5e07ef66d729954db42bef9b6251013af7cc14aa
sbmthakur <sbmthakur@gmail.com>
2020-11-01 16:58:53 +0000

    Deploying to gh-pages from  @ 2db0e56ea55f4366ef8aed02bcf9c110a86cbd3c 🚀

---
+++ _next/static/FJzhu5IUOLgwtCrvtw9aP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-5951b5d3769df2d2fd38.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
